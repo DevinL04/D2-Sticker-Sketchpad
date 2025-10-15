dad03a23393f95346d33da2ee0b82d2351be24f0
DevinL04 <123677317+DevinL04@users.noreply.github.com>
2025-10-15 17:11:00 +0000

    Added Canvas (Step 1 Complete)
    Co-authored-by: mrpoopers <mrpoopers@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+<h1 class="title">Welcome to Sketchpad</h1>
+
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+ <canvas id="myCanvas"></canvas>
+
 `;
